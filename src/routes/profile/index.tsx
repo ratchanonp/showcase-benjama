9e1ee6b764f254f747ab0776e6fe97542f74a43d
Ratchanon Panmas <28267945+ratchanonp@users.noreply.github.com>
2025-01-01 17:39:13 +0700

    refactor: simplify import path for ProfilePage component
    Update the import statement for the ProfilePage component to use an 
    absolute path instead of a relative path. This change improves code 
    readability and maintainability by reducing the complexity of the 
    import structure.

--- src/routes/profile/index.tsx
+++ src/routes/profile/index.tsx
@@ -5,9 +5,7 @@ import {
 } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/profile/")({
-  component: lazyRouteComponent(
-    () => import("../../modules/Profile/ProfilePage")
-  ),
+  component: lazyRouteComponent(() => import("@/modules/Profile/ProfilePage")),
   beforeLoad: ({ context, location }) => {
     if (!context.auth.isAuthenticated) {
       throw redirect({

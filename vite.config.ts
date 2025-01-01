18a62719beda35aa520e08377c25c993c3523d0d
Ratchanon Panmas <28267945+ratchanonp@users.noreply.github.com>
2025-01-01 15:58:44 +0700

    feat: implement new user authentication flow
    Add a new user authentication flow to enhance security and 
    improve user experience. Update login and registration 
    components to utilize the new API endpoints. Refactor 
    existing code to streamline the authentication process 
    and ensure compatibility with the latest backend changes.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import {
+  TanStackRouterCodeSplitterVite,
+  TanStackRouterVite,
+} from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite(),],
+  plugins: [react(), TanStackRouterVite(), TanStackRouterCodeSplitterVite()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
